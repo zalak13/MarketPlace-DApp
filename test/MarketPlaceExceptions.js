ba5a7061a9b7d7d1922b6e7441541fa1f0c0f229
Zalak Shah <zalak13@gmail.com>
2017-10-11 00:39:09 -0400

    Updated the backend code to sell and buy multiple items.

--- test/MarketPlaceExceptions.js
+++ test/MarketPlaceExceptions.js
@@ -6,109 +6,131 @@ contract('MarketPlace', function(accounts) {
   var marketPlaceInstance;
   var seller = accounts[1];
   var buyer = accounts[2];
+  var articleId = 1;
   var articleName = "article 1";
   var articleDescription = "Description for article 1";
   var articlePrice = 10;
 
-  // Test case: no article for sale yet
+  // Test case: buying an article when no article for sale yet
   it("should throw an exception if you try to buy an article when there is no article for sale", function() {
     return MarketPlace.deployed().then(function(instance) {
-      marketPlaceInstance = instance;
-      return marketPlaceInstance.buyArticle({
-        from: buyer,
-        value: web3.toWei(articlePrice, "ether")
+        marketPlaceInstance = instance;
+        return marketPlaceInstance.buyArticle(articleId, {
+          from: buyer,
+          value: web3.toWei(articlePrice, "ether")
+        });
+      }).then(assert.fail)
+      .catch(function(error) {
+        assert(error.message.indexOf('invalid opcode') >= 0, "error message must contain invalid opcode");
+      }).then(function() {
+        return marketPlaceInstance.getNumberOfArticles();
+      }).then(function(data) {
+        //make sure sure the contract state was not altered
+        assert.equal(data.toNumber(), 0, "number of articles must be zero");
       });
-    }).then(assert.fail)
-    .catch(function(error) {
-      assert(error.message.indexOf('invalid opcode') >= 0, error.message.indexOf('invalid opcode') >= 0);
-    }).then(function() {
-      return marketPlaceInstance.getArticle.call();
-    }).then(function(data) {
-      //make sure sure the contract state was not altered
-      assert.equal(data[0], 0x0, "seller must be empty");
-      assert.equal(data[1], 0x0, "buyer must be empty");
-      assert.equal(data[2], '', "article name must empty");
-      assert.equal(data[3], '', "article description must be empty");
-      assert.equal(data[4].toNumber(), 0, "article price must be 0");
-    });
+  });
+
+  // Test case: buying an article that does not exist
+  it("should throw an exception if you try to buy an article that does not exist", function() {
+    return MarketPlace.deployed().then(function(instance) {
+        marketPlaceInstance = instance;
+        return marketPlaceInstance.sellArticle(articleName, articleDescription, web3.toWei(articlePrice, "ether"), {
+          from: seller
+        });
+      }).then(function(receipt) {
+        return marketPlaceInstance.buyArticle(2, {
+          from: buyer,
+          value: web3.toWei(articlePrice, "ether")
+        });
+      }).then(assert.fail)
+      .catch(function(error) {
+        assert(error.message.indexOf('invalid opcode') >= 0, "error message must contain invalid opcode");
+      }).then(function() {
+        return marketPlaceInstance.articles(articleId);
+      }).then(function(data) {
+        assert.equal(data[0].toNumber(), articleId, "article id must be " + articleId);
+        assert.equal(data[1], seller, "seller must be " + seller);
+        assert.equal(data[2], 0x0, "buyer must be empty");
+        assert.equal(data[3], articleName, "article name must be " + articleName);
+        assert.equal(data[4], articleDescription, "article description must be " + articleDescription);
+        assert.equal(data[5].toNumber(), web3.toWei(articlePrice, "ether"), "article price must be " + web3.toWei(articlePrice, "ether"));
+      });
   });
 
   // Test case: buying an article you are selling
   it("should throw an exception if you try to buy your own article", function() {
     return MarketPlace.deployed().then(function(instance) {
-      marketPlaceInstance = instance;
-      return marketPlaceInstance.sellArticle(articleName, articleDescription, web3.toWei(articlePrice, "ether"), {
-        from: seller
+        marketPlaceInstance = instance;
+        return marketPlaceInstance.buyArticle(articleId, {
+          from: seller,
+          value: web3.toWei(articlePrice, "ether")
+        });
+      }).then(assert.fail)
+      .catch(function(error) {
+        assert(error.message.indexOf('invalid opcode') >= 0, "error message must contain invalid opcode");
+      }).then(function() {
+        return marketPlaceInstance.articles(articleId);
+      }).then(function(data) {
+        //make sure sure the contract state was not altered
+        assert.equal(data[0].toNumber(), articleId, "article id must be " + articleId);
+        assert.equal(data[1], seller, "seller must be " + seller);
+        assert.equal(data[2], 0x0, "buyer must be empty");
+        assert.equal(data[3], articleName, "article name must be " + articleName);
+        assert.equal(data[4], articleDescription, "article description must be " + articleDescription);
+        assert.equal(data[5].toNumber(), web3.toWei(articlePrice, "ether"), "article price must be " + web3.toWei(articlePrice, "ether"));
       });
-    }).then(function(receipt) {
-      return marketPlaceInstance.buyArticle({
-        from: seller,
-        value: web3.toWei(articlePrice, "ether")
-      });
-    }).then(assert.fail)
-    .catch(function(error) {
-      assert(error.message.indexOf('invalid opcode') >= 0, error.message.indexOf('invalid opcode') >= 0);
-    }).then(function() {
-      return marketPlaceInstance.getArticle.call();
-    }).then(function(data) {
-      //make sure sure the contract state was not altered
-      assert.equal(data[0], seller, "seller must be " + seller);
-      assert.equal(data[1], 0x0, "buyer must be empty");
-      assert.equal(data[2], articleName, "article name must be " + articleName);
-      assert.equal(data[3], articleDescription, "article description must be " + articleDescription);
-      assert.equal(data[4].toNumber(), web3.toWei(articlePrice, "ether"), "article price must be " + web3.toWei(articlePrice, "ether"));
-    });
   });
 
   // Test case: incorrect value
   it("should throw an exception if you try to buy an article for a value different from its price", function() {
     return MarketPlace.deployed().then(function(instance) {
-      marketPlaceInstance = instance;
-      return marketPlaceInstance.buyArticle({
-        from: buyer,
-        value: web3.toWei(articlePrice + 1, "ether")
+        marketPlaceInstance = instance;
+        return marketPlaceInstance.buyArticle(articleId, {
+          from: buyer,
+          value: web3.toWei(articlePrice + 1, "ether")
+        });
+      }).then(assert.fail)
+      .catch(function(error) {
+        assert(error.message.indexOf('invalid opcode') >= 0, "error message must contain invalid opcode");
+      }).then(function() {
+        return marketPlaceInstance.articles(articleId);
+      }).then(function(data) {
+        //make sure sure the contract state was not altered
+        assert.equal(data[0].toNumber(), articleId, "article id must be " + articleId);
+        assert.equal(data[1], seller, "seller must be " + seller);
+        assert.equal(data[2], 0x0, "buyer must be empty");
+        assert.equal(data[3], articleName, "article name must be " + articleName);
+        assert.equal(data[4], articleDescription, "article description must be " + articleDescription);
+        assert.equal(data[5].toNumber(), web3.toWei(articlePrice, "ether"), "article price must be " + web3.toWei(articlePrice, "ether"));
       });
-    }).then(assert.fail)
-    .catch(function(error) {
-      assert(error.message.indexOf('invalid opcode') >= 0, error.message.indexOf('invalid opcode') >= 0);
-    }).then(function() {
-      return marketPlaceInstance.getArticle.call();
-    }).then(function(data) {
-      //make sure sure the contract state was not altered
-      assert.equal(data[0], seller, "seller must be " + seller);
-      assert.equal(data[1], 0x0, "buyer must be empty");
-      assert.equal(data[2], articleName, "article name must be " + articleName);
-      assert.equal(data[3], articleDescription, "article description must be " + articleDescription);
-      assert.equal(data[4].toNumber(), web3.toWei(articlePrice, "ether"), "article price must be " + web3.toWei(articlePrice, "ether"));
-    });
   });
 
   // Test case: article has already been sold
   it("should throw an exception if you try to buy an article that has already been sold", function() {
     return MarketPlace.deployed().then(function(instance) {
-      marketPlaceInstance = instance;
-      return marketPlaceInstance.buyArticle({
-        from: buyer,
-        value: web3.toWei(articlePrice, "ether")
+        marketPlaceInstance = instance;
+        return marketPlaceInstance.buyArticle(articleId, {
+          from: buyer,
+          value: web3.toWei(articlePrice, "ether")
+        });
+      }).then(function() {
+        return marketPlaceInstance.buyArticle(articleId, {
+          from: web3.eth.accounts[0],
+          value: web3.toWei(articlePrice, "ether")
+        });
+      }).then(assert.fail)
+      .catch(function(error) {
+        assert(error.message.indexOf('invalid opcode') >= 0, "error message must contain invalid opcode");
+      }).then(function() {
+        return marketPlaceInstance.articles(articleId);
+      }).then(function(data) {
+        //make sure sure the contract state was not altered
+        assert.equal(data[0].toNumber(), articleId, "article id must be " + articleId);
+        assert.equal(data[1], seller, "seller must be " + seller);
+        assert.equal(data[2], buyer, "buyer must be " + buyer);
+        assert.equal(data[3], articleName, "article name must be " + articleName);
+        assert.equal(data[4], articleDescription, "article description must be " + articleDescription);
+        assert.equal(data[5].toNumber(), web3.toWei(articlePrice, "ether"), "article price must be " + web3.toWei(articlePrice, "ether"));
       });
-    }).then(function() {
-      return marketPlaceInstance.buyArticle({
-        from: web3.eth.accounts[0],
-        value: web3.toWei(articlePrice, "ether")
-      });
-    }).then(assert.fail)
-    .catch(function(error) {
-      assert(error.message.indexOf('invalid opcode') >= 0, error.message.indexOf('invalid opcode') >= 0);
-    }).then(function() {
-      return marketPlaceInstance.getArticle.call();
-    }).then(function(data) {
-      //make sure sure the contract state was not altered
-      assert.equal(data[0], seller, "seller must be " + seller);
-      assert.equal(data[1], buyer, "buyer must be empty");
-      assert.equal(data[2], articleName, "article name must be " + articleName);
-      assert.equal(data[3], articleDescription, "article description must be " + articleDescription);
-      assert.equal(data[4].toNumber(), web3.toWei(articlePrice, "ether"), "article price must be " + web3.toWei(articlePrice, "ether"));
-    });
   });
-
 });
--- test/MarketPlaceRightWay.js
+++ test/MarketPlaceRightWay.js
@@ -7,100 +7,148 @@ contract('MarketPlace',function(accounts){
   var marketPlaceInstance;
   var seller = accounts[1];
   var buyer = accounts[2];
-  var articleName = "article 1";
-  var articleDescription = "Description for article 1";
-  var articlePrice = 10;
+  var articleName1 = "article 1";
+  var articleDescription1 = "Description for article 1";
+  var articlePrice1 = 10;
+  var articleName2 = "article 2";
+  var articleDescription2 = "Description for article 2";
+  var articlePrice2 = 20;
   var watcher;
   var sellerBalanceBeforeBuy, sellerBalanceAfterBuy;
   var buyerBalanceBeforeBuy, buyerBalanceAfterBuy;
 
-  //Test case: check initial values
-  it("should be initialized with empty values",function(){
-    return MarketPlace.deployed().then(function(instance){
-      return instance.getArticle.call();
+  // Test case: check initial values
+  it("should be initialized with empty values", function() {
+    return MarketPlace.deployed().then(function(instance) {
+      marketPlaceInstance = instance;
+      return marketPlaceInstance.getNumberOfArticles();
+    }).then(function(data) {
+      assert.equal(data, 0x0, "number of articles must be zero");
+      return marketPlaceInstance.getArticlesForSale();
     }).then(function(data){
-      assert.equal(data[0],0x0,"seller must be empty");
-      assert.equal(data[1],0x0,"buyer must be empty");
-      assert.equal(data[2],'',"article name must be empty");
-      assert.equal(data[3],'',"description must be empty");
-      assert.equal(data[4].toNumber(),0,"article price must be zero");
+      assert.equal(data.length, 0, "articles for sale should be empty");
     });
   });
 
 
 
-  //Test case: sell an article
-  it("should sell an article",function(){
-    return MarketPlace.deployed().then(function(instance){
+  // Test case: sell a first article
+  it("should let us sell a first article", function() {
+    return MarketPlace.deployed().then(function(instance) {
       marketPlaceInstance = instance;
-      return marketPlaceInstance.sellArticle(articleName, articleDescription,web3.toWei(articlePrice,"ether"),{from: seller});
-    }).then(function(){
-        return marketPlaceInstance.getArticle.call();
-    }).then(function(data){
-        assert.equal(data[0],seller,"seller must be "+ seller);
-        assert.equal(data[1],0x0,"buyer must be "+ buyer);
-        assert.equal(data[2],articleName,"article name must be "+articleName);
-        assert.equal(data[3],articleDescription,"description must be "+ articleDescription);
-        assert.equal(data[4].toNumber(),web3.toWei(articlePrice,"ether"),"article price must be "+ web3.toWei(articlePrice,"ether"));
+      return marketPlaceInstance.sellArticle(articleName1, articleDescription1, web3.toWei(articlePrice1, "ether"), {
+        from: seller
       });
-    });
-
-  // Test case: should check events
-  it("should trigger an event when a new article is sold", function() {
-    return MarketPlace.deployed().then(function(instance) {
-      marketPlaceInstance = instance;
-      watcher = marketPlaceInstance.sellArticleEvent();
-      return marketPlaceInstance.sellArticle(
-        articleName,
-        articleDescription,
-        web3.toWei(articlePrice, "ether"), {
-          from: seller
-        }
-      );
     }).then(function(receipt) {
+      //check event
       assert.equal(receipt.logs.length, 1, "should have received one event");
+      assert.equal(receipt.logs[0].event, "sellArticleEvent", "event name should be sellArticleEvent");
+      assert.equal(receipt.logs[0].args._id.toNumber(), 1, "id must be 1");
       assert.equal(receipt.logs[0].args._seller, seller, "seller must be " + seller);
-      assert.equal(receipt.logs[0].args._name, articleName, "article name must be " + articleName);
-      assert.equal(receipt.logs[0].args._price.toNumber(), web3.toWei(articlePrice, "ether"), "article price must be " + web3.toWei(articlePrice, "ether"));
+      assert.equal(receipt.logs[0].args._name, articleName1, "article name must be " + articleName1);
+      assert.equal(receipt.logs[0].args._price.toNumber(), web3.toWei(articlePrice1, "ether"), "article price must be " + web3.toWei(articlePrice1, "ether"));
+
+      return marketPlaceInstance.getNumberOfArticles();
+    }).then(function(data) {
+      assert.equal(data, 1, "number of articles must be one");
+
+      return marketPlaceInstance.getArticlesForSale();
+    }).then(function(data) {
+      assert.equal(data.length, 1, "there must now be 1 article for sale");
+      articleId = data[0].toNumber();
+      assert.equal(articleId, 1, "article id must be 1");
+
+      return marketPlaceInstance.articles(articleId);
+    }).then(function(data) {
+      assert.equal(data[0].toNumber(), 1, "article id must be 1");
+      assert.equal(data[1], seller, "seller must be " + seller);
+      assert.equal(data[2], 0x0, "buyer must be empty");
+      assert.equal(data[3], articleName1, "article name must be " + articleName1);
+      assert.equal(data[4], articleDescription1, "article description must be " + articleDescription1);
+      assert.equal(data[5].toNumber(), web3.toWei(articlePrice1, "ether"), "article price must be " + web3.toWei(articlePrice1, "ether"));
     });
   });
 
-  // Test case: buy an article
-  it("should buy an article", function() {
+  // Test case: sell a second article
+  it("should let us sell a second article", function() {
     return MarketPlace.deployed().then(function(instance) {
       marketPlaceInstance = instance;
+      return marketPlaceInstance.sellArticle(articleName2, articleDescription2, web3.toWei(articlePrice2, "ether"), {
+        from: seller
+      });
+    }).then(function(receipt) {
+      assert.equal(receipt.logs.length, 1, "one event should have been triggered");
+      assert.equal(receipt.logs[0].event, "sellArticleEvent", "event should be sellArticleEvent");
+      assert.equal(receipt.logs[0].args._id.toNumber(), 2, "id must be 2");
+      assert.equal(receipt.logs[0].args._seller, seller, "event seller must be " + seller);
+      assert.equal(receipt.logs[0].args._name, articleName2, "event article name must be " + articleName2);
+      assert.equal(receipt.logs[0].args._price.toNumber(), web3.toWei(articlePrice2, "ether"), "event article price must be " + web3.toWei(articlePrice2, "ether"));
 
-      //record balances of seller and buyer before the buy
+      return marketPlaceInstance.getNumberOfArticles();
+    }).then(function(data) {
+      assert.equal(data, 2, "number of articles must be two");
+
+      return marketPlaceInstance.getArticlesForSale();
+    }).then(function(data) {
+      assert.equal(data.length, 2, "there must now be 2 articles for sale");
+      articleId = data[1].toNumber();
+      assert.equal(articleId, 2, "article id must be 2");
+
+      return marketPlaceInstance.articles(articleId);
+    }).then(function(data) {
+      assert.equal(data[0].toNumber(), 2, "article id must be 2");
+      assert.equal(data[1], seller, "seller must be " + seller);
+      assert.equal(data[2], 0x0, "buyer must be empty");
+      assert.equal(data[3], articleName2, "article name must be " + articleName2);
+      assert.equal(data[4], articleDescription2, "article description must be " + articleDescription2);
+      assert.equal(data[5].toNumber(), web3.toWei(articlePrice2, "ether"), "article price must be " + web3.toWei(articlePrice2, "ether"));
+    });
+  });
+
+
+  // Test case: buy the first article
+  it("should let us buy the first article", function() {
+    return MarketPlace.deployed().then(function(instance) {
+      marketPlaceInstance = instance;
+      articleId = 1;
+
+      // record balances of seller and buyer before the buy
       sellerBalanceBeforeBuy = web3.fromWei(web3.eth.getBalance(seller), "ether").toNumber();
       buyerBalanceBeforeBuy = web3.fromWei(web3.eth.getBalance(buyer), "ether").toNumber();
 
-      return marketPlaceInstance.buyArticle({
+      return marketPlaceInstance.buyArticle(articleId, {
         from: buyer,
-        value: web3.toWei(articlePrice, "ether")
+        value: web3.toWei(articlePrice1, "ether")
       });
     }).then(function(receipt) {
       assert.equal(receipt.logs.length, 1, "one event should have been triggered");
       assert.equal(receipt.logs[0].event, "buyArticleEvent", "event should be buyArticleEvent");
+      assert.equal(receipt.logs[0].args._id.toNumber(), articleId, "articleId must be " + articleId);
       assert.equal(receipt.logs[0].args._seller, seller, "event seller must be " + seller);
       assert.equal(receipt.logs[0].args._buyer, buyer, "event buyer must be " + buyer);
-      assert.equal(receipt.logs[0].args._name, articleName, "event article name must be " + articleName);
-      assert.equal(receipt.logs[0].args._price.toNumber(), web3.toWei(articlePrice, "ether"), "event article price must be " + web3.toWei(articlePrice, "ether"));
+      assert.equal(receipt.logs[0].args._name, articleName1, "event article name must be " + articleName1);
+      assert.equal(receipt.logs[0].args._price.toNumber(), web3.toWei(articlePrice1, "ether"), "event article price must be " + web3.toWei(articlePrice1, "ether"));
 
-      //record balances of buyer and seller after the buy
+      // record balances of buyer and seller after the buy
       sellerBalanceAfterBuy = web3.fromWei(web3.eth.getBalance(seller), "ether").toNumber();
       buyerBalanceAfterBuy = web3.fromWei(web3.eth.getBalance(buyer), "ether").toNumber();
 
       //check the effect of buy on balances of buyer and seller, accounting for gas
-      assert(sellerBalanceAfterBuy == sellerBalanceBeforeBuy + articlePrice, "seller should have earned " + articlePrice + " ETH");
-      assert(buyerBalanceAfterBuy <= buyerBalanceBeforeBuy - articlePrice, "buyer should have spent " + articlePrice + " ETH");
+      assert(sellerBalanceAfterBuy == sellerBalanceBeforeBuy + articlePrice1, "seller should have earned " + articlePrice1 + " ETH");
+      assert(buyerBalanceAfterBuy <= buyerBalanceBeforeBuy - articlePrice1, "buyer should have spent " + articlePrice1 + " ETH");
 
-      return marketPlaceInstance.getArticle.call();
+      return marketPlaceInstance.articles(articleId);
     }).then(function(data) {
-      assert.equal(data[0], seller, "seller must be " + seller);
-      assert.equal(data[1], buyer, "buyer must be " + buyer);
-      assert.equal(data[2], articleName, "article name must be " + articleName);
-      assert.equal(data[3], articleDescription, "article description must be " + articleDescription);
-      assert.equal(data[4].toNumber(), web3.toWei(articlePrice, "ether"), "article price must be " + web3.toWei(articlePrice, "ether"));
+      assert.equal(data[0].toNumber(), 1, "article id must be 1");
+      assert.equal(data[1], seller, "seller must be " + seller);
+      assert.equal(data[2], buyer, "buyer must be " + buyer);
+      assert.equal(data[3], articleName1, "article name must be " + articleName1);
+      assert.equal(data[4], articleDescription1, "article description must be " + articleDescription1);
+      assert.equal(data[5].toNumber(), web3.toWei(articlePrice1, "ether"), "article price must be " + web3.toWei(articlePrice1, "ether"));
+
+      return marketPlaceInstance.getArticlesForSale();
+    }).then(function(data) {
+      assert(data.length, 1, "there should now be only one article left for sale");
     });
   });
 });
