6bffad47934f62cce38ca6fcedbbaa925b1cd9ca
Zalak Shah <zalak13@gmail.com>
2017-10-15 21:12:54 -0400

    Adding frontend files to Github Pages

--- truffle.js
+++ truffle.js
@@ -4,6 +4,11 @@ module.exports = {
       host: "localhost",
       port: 8545,
       network_id: "*" // Match any network id
+    },
+    rinkeby: {
+      host:"localhost",
+      port: 8545,
+      network_id: 4
     }
   }
 };
